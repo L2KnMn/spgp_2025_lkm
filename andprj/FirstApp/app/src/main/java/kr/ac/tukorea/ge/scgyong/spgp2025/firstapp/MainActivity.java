66b2bde33e6ce60153d633aefb0db9e3bf87dbc8
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-17 02:34:44 +0900

    ViewBinding 을 이용하면 코드가 간결해진다. 귀찮은 일을 툴이 해 준다.

--- andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/firstapp/MainActivity.java
+++ andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/firstapp/MainActivity.java
@@ -5,44 +5,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea.ge.scgyong.spgp2025.firstapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity  {
 
-    private TextView mainTextView;
-    private Button mainButton;
-    private Button pushMeButton;
-    private TextView subTextView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mainTextView = findViewById(R.id.mainTextView);
-
         // 추가 선언 과정 설명.
-        subTextView = findViewById(R.id.subTextView);
-
-        mainButton = findViewById(R.id.mainButton);
-        mainButton.setOnClickListener(m_mainButtonListener);
-
-        pushMeButton = findViewById(R.id.pushMeButton);
-        pushMeButton.setOnClickListener(m_pushMeButtonListener);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.mainButton.setOnClickListener(m_mainButtonListener);
+        binding.pushMeButton.setOnClickListener(m_pushMeButtonListener);
     }
 
     private View.OnClickListener m_mainButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            mainTextView.setText("Main Button Clicked");
-            subTextView.setText("Main is 4 characters long");
+            binding.mainTextView.setText("Main Button Clicked");
+            binding.subTextView.setText("Main is 4 characters long");
         }
     };
     private View.OnClickListener m_pushMeButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            mainTextView.setText("PushMe Button Clicked");
-            subTextView.setText("PushMe is 6 characters long");
+            binding.mainTextView.setText("PushMe Button Clicked");
+            binding.subTextView.setText("PushMe is 6 characters long");
         }
     };
 }
