590e79a2d66cccd970a54a02c5495e192caa5005
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-17 01:56:41 +0900

    Event Listener 연결하는 방법 #1 "그 일이 생기면 나한테 알려줘"

--- andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/firstapp/MainActivity.java
+++ andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/firstapp/MainActivity.java
@@ -1,21 +1,26 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.firstapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.mainTextView);
-        tv.setText("Program Started");
-
         Button btn = findViewById(R.id.mainButton);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView tv = findViewById(R.id.mainTextView);
+        tv.setText("Main Button Clicked");
     }
 }
