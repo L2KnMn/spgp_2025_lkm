20f14411fb61fd5ec923d2287a4dc16469f2ab26
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-17 01:27:36 +0900

    id 를 이용하여 findViewById 를 하고 나면 TextView 를 얻을 수 있고 이 개체에 대한 여러 속성을 RunTime 에 변경이 가능하다.

--- andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/firstapp/MainActivity.java
+++ andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/firstapp/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.firstapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.mainTextView);
+        tv.setText("Program Started");
     }
 }
