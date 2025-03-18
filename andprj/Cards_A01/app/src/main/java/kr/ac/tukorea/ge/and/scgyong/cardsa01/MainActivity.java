bd9b39f3e3ca8b7a7fd835c099f8a41432b6dd03
asd <sad@ads>
2025-03-18 12:15:52 +0900

    16개의 버튼에게 각각 int imageResourceId 를 tag 로 달고 눌렸을 때 tag 를 꺼내서 이미지 설정한다.

--- andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa01/MainActivity.java
+++ andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa01/MainActivity.java
@@ -16,11 +16,31 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton previousCardButton;
     private @NonNull ActivityMainBinding ui;
 
+    private static final int[] CARD_RES_IDS = {
+            R.mipmap.card_as, R.mipmap.card_2c, R.mipmap.card_3d, R.mipmap.card_4h,
+            R.mipmap.card_5s, R.mipmap.card_jc, R.mipmap.card_qh, R.mipmap.card_kd,
+            R.mipmap.card_as, R.mipmap.card_2c, R.mipmap.card_3d, R.mipmap.card_4h,
+            R.mipmap.card_5s, R.mipmap.card_jc, R.mipmap.card_qh, R.mipmap.card_kd,
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ui = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(ui.getRoot());
+
+        ImageButton[] buttons = new ImageButton[]{
+                ui.card00, ui.card01, ui.card02, ui.card03,
+                ui.card10, ui.card11, ui.card12, ui.card13,
+                ui.card20, ui.card21, ui.card22, ui.card23,
+                ui.card30, ui.card31, ui.card32, ui.card33,
+        };
+        for (int i = 0; i < CARD_RES_IDS.length; i++) {
+            ImageButton btn = buttons[i];
+            int resId = CARD_RES_IDS[i];
+            Integer resourceIdInteger = resId;
+            btn.setTag(resourceIdInteger);
+        }
     }
 
     public void onBtnCard(View view) {
@@ -32,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ImageButton btn = (ImageButton) view;
-        btn.setImageResource(R.mipmap.card_as);
+        int resId = (Integer) btn.getTag();
+        btn.setImageResource(resId);
 
         previousCardButton = btn;
     }
